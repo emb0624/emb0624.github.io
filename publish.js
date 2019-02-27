0f6a2e6cf6e08303ed70eb702583a23599b741ac
Emmanuel Morales <emorales0024@gmail.com>
2019-02-27 12:45:50 -0600

    Fix publish source dir

--- publish.js
+++ publish.js
@@ -3,6 +3,6 @@ var ghpages = require('gh-pages');
 /**
  * This task pushes to the `master` branch of the configured `repo`.
  */
-ghpages.publish('public', {
+ghpages.publish('build', {
   branch: 'master'
 }, function() {});
